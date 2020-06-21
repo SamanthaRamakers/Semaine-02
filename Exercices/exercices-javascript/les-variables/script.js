1d2c2d94c61e921e1347c31fd3bda14aba231491
SamanthaRamakers <66729697+SamanthaRamakers@users.noreply.github.com>
2020-06-21 14:43:15 +0200

    update cv

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,16 @@
+let helloWorld = 'Hello world';
+console.log(helloWorld);
+let name = "Bonjour je m'appelle";
+let surname = 'samantha';
+console.log(name + ' ' + surname);
+let string = "j'ai";
+let number = 34;
+let year = 'ans';
+console.log(name + ' ' + surname + ' ' + string + ' ' + number + ' ' + year);
+let array = ['Ola', 'Sam', 'Tomie', 'Obi', 'Olatoye', 'Yetunde', 'Olusegun'];
+console.log(array[6]);
+console.log(array[1]);
+// mon prenom 1.5
+const student = { name: 'Ramakers', firstname: 'Sam', age: 34 };
+console.log('student:', student);
+console.log('student:', 'Sam');
