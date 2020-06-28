33c966995f95f9426f2c0d2ba6499f6c1ca5a886
SamanthaRamakers <66729697+SamanthaRamakers@users.noreply.github.com>
2020-06-28 18:05:53 +0200

    Conditions exs

---
+++ Exercices/exercices-javascript/les-conditions/script.js
@@ -0,0 +1,15 @@
+let leftNumber = 8;
+let rightNumber = 10;
+if (rightNumber < leftNumber) {
+  result = 'true';
+} else {
+  result = 'false';
+}
+if (leftNumber > rightNumber) {
+  result = 'true';
+} else result = 'false';
+if (letNumber / 2) {
+  result = 'true';
+} else {
+  result = 'false';
+}
